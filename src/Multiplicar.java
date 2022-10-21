bb24e4bd1acbdb72856c159e11d0a4e7f38d7ab1
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2022-10-21 12:30:50 +0200

    Multiplicar

---
+++ src/Multiplicar.java
@@ -0,0 +1,16 @@
+public class Multiplicar {
+    
+    public static void main(String[] args) {
+        int numero = 10;        
+
+        for (int i = 0; i < numero; i++) {
+            System.out.println(i+" X "+numero);
+            numero*=i;
+
+            /*crear 10 hilos que devuelvan el resultado*/
+
+        }
+
+    }
+
+}
